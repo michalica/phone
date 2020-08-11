d76cd6c89cfb6659a3f0c42f065a3c709f865261
Tomas <michalica.t@gmail.com>
2020-08-11 09:04:42 +0200

    adding cors

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,13 @@ import Router from "koa-router"
 import bodyParser from 'koa-bodyparser';
 import {AppRoutes} from "./router/routes";
 import {errorHandler} from "./middlewares/errorHandler/errorHandler";
+const cors = require('@koa/cors');
 
 export default class App{
     public init(): void
     {
         const app = new Koa();
+        app.use(cors());
         const router = new Router();
 
         AppRoutes.forEach(route => router[route.method](route.path, (ctx: Context, next: Next) => {
